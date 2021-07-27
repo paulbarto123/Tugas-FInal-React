fa45893866c13dcd32752021a8abbe72a68f075a
Paulus Siahaan <siahaan.paulus123@gmail.com>
2021-07-27 19:56:52 +0700

    commit_initial

---
+++ react-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react'
+//import Chart from './Atom/Chart/Chart'
+import Navbar from './Atom/Navbar'
+import Register from './Atom/Modal/Register'
+
+export default class App extends React.Component{
+
+    render(){
+      return (
+        <>
+        <Navbar />
+        <Register />
+        </>
+      );
+    }
+} 
+  
+
